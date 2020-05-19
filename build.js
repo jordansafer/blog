bd0801ae2ebc71b599dbdd4fbbb16870a9bdc256
Jordan Safer <jordansafer2@gmail.com>
2020-05-19 18:30:01 +0000

    cleanout and eslint

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-'use strict';
+"use strict"
 
 /**
 * Metalsmith build file
@@ -8,27 +8,25 @@
 */
 
 // variables
-const baseUrl = '/';
-const siteName = 'Example Metalsmith Blog';
-const siteRoot = 'https://www.example.com/';
+const baseUrl = "/"
+const siteName = "Example Metalsmith Blog"
+const siteRoot = "https://www.example.com/"
 const navItems = [
-    { href: 'index.html', text: 'Posts' },
-    { href: 'first-page.html', text: 'First Page' },
-    { href: 'second-page.html', text: 'Second Page' },
-];
+    { href: "index.html", text: "Posts" }
+]
 
 // metalsmith plugins
-const collections = require('metalsmith-collections');
-const dateFormatter = require('metalsmith-date-formatter');
-const htmlMinifier = require('metalsmith-html-minifier');
-const layouts = require('metalsmith-layouts');
-const markdown = require('metalsmith-markdown');
-const metalsmith = require('metalsmith');
-const more = require('metalsmith-more');
-const pagination = require('metalsmith-pagination');
-const pug = require('metalsmith-pug');
-const sass = require('metalsmith-sass');
-const sitemap = require('metalsmith-sitemap');
+const collections = require("metalsmith-collections")
+const dateFormatter = require("metalsmith-date-formatter")
+const htmlMinifier = require("metalsmith-html-minifier")
+const layouts = require("metalsmith-layouts")
+const markdown = require("metalsmith-markdown")
+const metalsmith = require("metalsmith")
+const more = require("metalsmith-more")
+const pagination = require("metalsmith-pagination")
+const pug = require("metalsmith-pug")
+const sass = require("metalsmith-sass")
+const sitemap = require("metalsmith-sitemap")
 
 // build configuration (order is important)
 metalsmith(__dirname)
@@ -39,19 +37,19 @@ metalsmith(__dirname)
             name: siteName,
         }
     })
-    .source('./src')
-    .destination('./public')
+    .source("./src")
+    .destination("./public")
     .use(dateFormatter({
         dates: [
             {
-                key: 'date',
-                format: 'YYYY-MM-DD'
+                key: "date",
+                format: "YYYY-MM-DD"
             }
         ]
     }))
     .use(markdown())
     .use(sass({
-        outputStyle: 'compressed'
+        outputStyle: "compressed"
     }))
     .use(pug({
         useMetadata: true
@@ -59,20 +57,20 @@ metalsmith(__dirname)
     .use(more())
     .use(collections({
         posts: {
-            pattern: 'posts/*/*.html',
-            sortBy: 'date',
+            pattern: "posts/*/*.html",
+            sortBy: "date",
             reverse: true
         },
     }))
     .use(pagination({
-        'collections.posts': {
-            layout: 'index.pug',
+        "collections.posts": {
+            layout: "index.pug",
             perPage: 4,
-            first: 'index.html',
+            first: "index.html",
             noPageOne: true,
-            path: 'index-:num.html',
+            path: "index-:num.html",
             pageMetadata: {
-                title: 'Posts'
+                title: "Posts"
             }
         },
     }))
@@ -83,8 +81,8 @@ metalsmith(__dirname)
     .use(htmlMinifier())
     .build(function (err) {
         if (err) {
-            console.log(err);
+            console.log(err)
         } else {
-            console.log('Build complete');
+            console.log("Build complete")
         }
-    });
+    })
